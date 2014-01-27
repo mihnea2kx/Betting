c47bbeb6551470620b289b843b73a78a6bfe60e8
mihnea2kx <mihnea2kx@yahoo.com>
2014-01-27 02:35:03 +0200

    com2

--- js/app.js
+++ js/app.js
@@ -4,25 +4,25 @@ $(document).ready(function(){
     $.getJSON(betapi,function(data) {
         $(data.emp_info).each(function () {
             if(this.Rezultat=='TBP'&&this.Valoare == 'Free'){
-            var output = "<li>" + this.Ora +"---"+ this.Sport +"---"+ this.Echipa1 + " vs " + this.Echipa2 + "---" + this.Tip + "</li>";
+            var output = "<li><p>" + this.Ora +"---"+ this.Sport +"---"+ this.Echipa1 + " vs " + this.Echipa2 + "---" + this.Tip + "</p></li>";
                 $('#aici').prepend(output);
-                $("li:first-child").addClass(this.Rezultat);
+                $("#aici li:first-child").addClass(this.Rezultat);
             }
         });
 
         $(data.emp_info).each(function () {
             if(this.Rezultat=='TBP'&&this.Valoare == 'Premium'){
-            var output = "<li>" + this.Ora +"---"+ this.Sport +"---"+ this.Echipa1 + " vs " + this.Echipa2 + "---" + this.Tip + "</li>";
+            var output = "<li><p>" + this.Ora +"---"+ this.Sport +"---"+ this.Echipa1 + " vs " + this.Echipa2 + "---" + this.Tip + "</p></li>";
                 $('#aici2').prepend(output);
-                $("li:first-child").addClass(this.Rezultat);
+                $("#aici2 li:first-child").addClass(this.Rezultat);
             }
         });
 
         $(data.emp_info).each(function () {
             if(this.Rezultat != 'TBP'){
-            var output = "<li>" + this.Echipa1 + " vs " + this.Echipa2 + "---" + this.Tip + "----->" + this.Rezultat + "</li>";
+            var output = "<li><p>" + this.Echipa1 + " vs " + this.Echipa2 + "---" + this.Tip + "----->" + this.Rezultat + "</p></li>";
                 $('#aici3').prepend(output);
-                $("li:first-child").addClass(this.Rezultat);
+                $("#aici3 li:first-child").addClass(this.Rezultat);
             }
         });
 
