a4c9c6b4ff4a630e71679bed4af9af0f1336a100
mihnea2kx <mihnea2kx@yahoo.com>
2014-01-29 14:26:37 +0200

    com15

--- js/app.js
+++ js/app.js
@@ -2,11 +2,18 @@ $(document).ready(function(){
     var betapi = "http://www.scripthold.com/betjoomla/connect.php";
     
     $.getJSON(betapi,function(data) {
+    //    $(data.emp_info).each(function () {
+    //        if(this.Rezultat=='TBP'&&this.Valoare == 'Free'){
+    //        var output = "<li><p>" + this.Ora.substring(0,5) +" -- "+ this.Sport +" -- "+ this.Echipa1 + " vs " + this.Echipa2 + " -- " + this.Tip + "</p></li>";
+   //             $('#aici').prepend(output);
+   //             $("#aici li:first-child").addClass(this.Rezultat);
+   //         }
+   //     });
         $(data.emp_info).each(function () {
             if(this.Rezultat=='TBP'&&this.Valoare == 'Free'){
-            var output = "<li><p>" + this.Ora.substring(0,5) +" -- "+ this.Sport +" -- "+ this.Echipa1 + " vs " + this.Echipa2 + " -- " + this.Tip + "</p></li>";
+            var output = "<tr><td rowspan=2>" +this.Ora.substring(0,5)+ "</td><td rowspan=2>" +this.Sport+ "</td><td>" +this.Echipa1+ "</td><td rowspan=2>" +this.Tip+ "</td></tr><tr><td>"  +this.Echipa2+"</td></tr>"
                 $('#aici').prepend(output);
-                $("#aici li:first-child").addClass(this.Rezultat);
+                //$("#aici2 tr:first-child").addClass(this.Rezultat);
             }
         });
 
